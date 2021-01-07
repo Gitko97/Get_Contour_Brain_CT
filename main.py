ec533bc9f5e5966b6c495db59ebd47fd279f7f87
Gitko97 <xcvdsf8216@gmail.com>
2021-01-07 18:19:33 +0900

    Hu 이미지를 AdaptiveThreshold 에서 일반 임계값(-100이 default)로 바이너리 이미지를 얻도록 변경

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+import cv2
 import numpy as np
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QFileDialog
@@ -16,24 +17,18 @@ class Controller(object):
     def __init__(self, file_extension=".dcm"):
         self.file_paths = []
         self.dicom_files = []
-        self.original_hu_images = []
         self.images_hu_pixels = []
-        self.normalized_image = []
         self.file_io = FileInOut(file_extension)
         self.image_preprocessor = Image_PreProcessor()
-        self.image_mean_pixel = None
         self.setting_value = {
-            "hu_boundary_value": -70,
-            "normalized_min_bound": -1000,
-            "normalized_max_bound": 600,
-            "contour_OTSU_boundary_value": 100,
+            "hu_boundary_value": -200,
             "brain_init_position": (250,145)
         }
         self.default_setting_value = self.setting_value
 
     def set_setting_value(self, setting_value):
         self.setting_value = setting_value
-        self.original_hu_images, self.images_hu_pixels, self.image_mean_pixel, self.normalized_image = self.get_images_pixels(self.dicom_files)
+        self.images_hu_pixels = self.image_preprocessor.get_pixels_hu(self.dicom_files)
 
     def start(self):
         app = QtWidgets.QApplication(sys.argv)
@@ -44,14 +39,13 @@ class Controller(object):
     def load_files(self):
         # selected_dir = QFileDialog.getExistingDirectory(None, caption='Choose Directory', directory=os.getcwd())
         # self.file_paths = self.file_io.search(selected_dir)
-        self.file_paths = self.file_io.search("/Users/joonhyoungjeon/Downloads/S005/CT")
+        self.file_paths = self.file_io.search("/Users/joonhyoungjeon/Downloads/20201221/S168/CT")
 
         self.ui.listView.clear()
         for filepath in self.file_paths:
             self.ui.listView.addItem(filepath[1])
         self.dicom_files = self.image_preprocessor.load_scan(self.file_paths)
-        self.original_hu_images, self.images_hu_pixels, self.image_mean_pixel, self.normalized_image = self.get_images_pixels(self.dicom_files)
-        self.image_shape = self.images_hu_pixels[0].shape
+        self.images_hu_pixels = self.image_preprocessor.get_pixels_hu(self.dicom_files)
 
     def add_files(self):
         selected_dir = QFileDialog.getExistingDirectory(None, caption='Choose Directory', directory=os.getcwd())
@@ -61,7 +55,7 @@ class Controller(object):
             self.ui.listView.addItem(filepath[1])
             self.dicom_files.extend(self.image_preprocessor.load_scan([filepath]))
 
-        self.original_hu_images, self.images_hu_pixels, self.image_mean_pixel, self.normalized_image = self.get_images_pixels(self.dicom_files)
+        self.images_hu_pixels = self.image_preprocessor.get_pixels_hu(self.dicom_files)
 
     def delete_files(self):
         for index, selected_index in enumerate(self.ui.listView.selectedIndexes()):
@@ -69,52 +63,32 @@ class Controller(object):
             self.dicom_files.pop(selected_index.row() - index)
             self.ui.listView.takeItem(selected_index.row() - index)
 
-        self.original_hu_images, self.images_hu_pixels, self.image_mean_pixel, self.normalized_image = self.get_images_pixels(self.dicom_files)
-
-    def get_images_pixels(self, dicom_files):
-        original_hu_image, images_hu_pixels = self.image_preprocessor.get_pixels_hu(dicom_files, hu_boundary_value=self.setting_value.get("hu_boundary_value"))
-        image_mean_pixel, normalized_image = self.image_preprocessor.normalize(images_hu_pixels, min_bound=self.setting_value.get("normalized_min_bound"), max_bound=self.setting_value.get("normalized_max_bound"))
-        return original_hu_image, images_hu_pixels, image_mean_pixel, normalized_image
+        self.images_hu_pixels = self.image_preprocessor.get_pixels_hu(self.dicom_files)
 
     def itemClicked(self):
         selected_index = self.ui.listView.currentRow()
-        brain_crop_images = self.dicom_preprocess(self.original_hu_images[selected_index],self.normalized_image[selected_index])
+        brain_crop_images = self.dicom_preprocess(self.images_hu_pixels[selected_index], self.images_hu_pixels[selected_index])
         plt.figure(num=self.ui.listView.currentItem().text(), figsize=(5, 3), dpi=200)
-        plt.subplot(1, 3, 1)
-        plt.axis('off')
-        plt.tight_layout()
-        plt.imshow(self.original_hu_images[selected_index], cmap='bone')
-        plt.title("Original Image")
-        plt.subplot(1, 3, 2)
+        plt.subplot(1, 2, 1)
         plt.axis('off')
         plt.tight_layout()
         plt.imshow(self.images_hu_pixels[selected_index], cmap='bone')
-        plt.title("Normalized Image")
-        plt.subplot(1, 3, 3)
+        plt.title("Original Image")
+        plt.subplot(1, 2, 2)
         plt.axis('off')
         plt.tight_layout()
-        plt.imshow(brain_crop_images[0], cmap='bone')
+        plt.imshow(brain_crop_images, cmap='bone')
         plt.title("Crop Image")
         plt.show()
 
+    def dicom_preprocess(self,paint_crop_image, cropping_image):
+        binary_image = self.image_preprocessor.get_binary_image_with_hu_value(cropping_image, hu_boundary_value=self.setting_value.get("hu_boundary_value"))
 
-    def dicom_preprocess(self, original_image,normalized_image):
-        brain_crop_images = []
-        contour_image = (normalized_image * 255).astype(np.uint8)  # 100 이라는 경계값으로 contour를 찾기 위해서 0~255로 값 바꿈
-        _, contours, hierarchy = self.image_preprocessor.find_dicom_Countour_OTSU(contour_image, contour_boundary_value=self.setting_value.get("contour_OTSU_boundary_value"))
+        _, contours, hierarchy = self.image_preprocessor.find_dicom_Countour(binary_image)
         brain_contour = self.image_preprocessor.find_brain_contour(contours=contours, hierarchy=hierarchy,
                                                               init_position=self.setting_value.get("brain_init_position"))
-        output = self.image_preprocessor.extract_image_with_contour(image=original_image, brain_contour=brain_contour)
-        brain_crop_images.append(output)
-        return np.array(brain_crop_images)
-
-    def dicom_preprocess_with_original_image(self, normalized_image, index):
-        contour_image = (normalized_image * 255).astype(np.uint8)  # 100 이라는 경계값으로 contour를 찾기 위해서 0~255로 값 바꿈
-        _, contours, hierarchy = self.image_preprocessor.find_dicom_Countour_OTSU(contour_image, contour_boundary_value=self.setting_value.get("contour_OTSU_boundary_value"))
-        brain_contour = self.image_preprocessor.find_brain_contour(contours=contours, hierarchy=hierarchy,
-                                                              init_position=self.setting_value.get("brain_init_position"))
-        output = self.image_preprocessor.extract_image_with_contour(image=self.dicom_files[index].pixel_array, brain_contour=brain_contour)
-        return output
+        output = self.image_preprocessor.extract_image_with_contour(image=paint_crop_image, brain_contour=brain_contour)
+        return np.array(output)
 
     def openSetting(self):
         selected_index = self.ui.listView.currentRow()
@@ -125,7 +99,7 @@ class Controller(object):
         setting_controller.start()
 
     def file_save(self):
-        croped_image = [self.dicom_preprocess(file.pixel_array, image) for file, image in zip(self.dicom_files, self.normalized_image)]
+        croped_image = [self.dicom_preprocess(file.pixel_array, image) for file, image in zip(self.dicom_files, self.images_hu_pixels)]
         for file, brain in zip(self.dicom_files, croped_image):
             file.PixelData = brain.astype(np.uint16).tobytes()
         self.file_io.save(self.file_paths, self.dicom_files)
--- preprocessor.py
+++ preprocessor.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+from pydicom.pixel_data_handlers import util
 
 
 class Image_PreProcessor(object):
@@ -13,45 +14,87 @@ class Image_PreProcessor(object):
 
         return slices
 
-    def get_pixels_hu(self, dicom_files, hu_boundary_value=-70):
-        image = np.stack([s.pixel_array for s in dicom_files])
-        image = image.astype(np.int16)
-        for slice_number in range(len(dicom_files)):
+    def get_pixels_hu(self, dicom_files):
+        # pydicom's apply_modality_lut method has similar function with this method
+        images = np.stack([s.pixel_array for s in dicom_files])
+        print(images.min(), images.max())
+        hu_pixels = []
+        for file, image in zip(dicom_files, images):
+            rescaled_arr = util.apply_modality_lut(image, file)
+            # windowed_rescaled_arr = util.apply_voi_lut(rescaled_arr, file, index=0)
+            hu_pixels.append(rescaled_arr)
+        return np.array(hu_pixels, dtype=np.int16)
 
-            intercept = dicom_files[slice_number].RescaleIntercept
-            slope = dicom_files[slice_number].RescaleSlope
-            if slope != 1:
-                image[slice_number] = slope * image[slice_number].astype(np.float64)
-                image[slice_number] = image[slice_number].astype(np.int16)
+    def get_binary_image_with_hu_value(self, hu_image, hu_boundary_value=-70):
+        binary_image = np.where(hu_image < hu_boundary_value, 0, 1)
 
-            image[slice_number] += np.int16(intercept)
-        original_hu_image = image.copy()
-        image[image <= hu_boundary_value] = image.min()
-        return np.array(original_hu_image, dtype=np.int16), np.array(image, dtype=np.int16)
+        return np.array(binary_image, dtype=np.int16)
 
-    def normalize(self, images, min_bound=-1000, max_bound=500, pixel_mean=None):
+    def window_image(self, image, window_center, window_width):
+        img_min = window_center - window_width // 2
+        img_max = window_center + window_width // 2
+        window_image = image.copy()
+        window_image[window_image < img_min] = img_min
+        window_image[window_image > img_max] = img_max
+
+        return window_image
+
+    def normalize(self, images, min_bound=-1000, max_bound=2000, pixel_mean=None):
         images = (images - min_bound) / (max_bound - min_bound)
         images[images > 1] = 1.
         images[images < 0] = 0.
-        if pixel_mean == None :
+        if pixel_mean == None:
             pixel_mean = images.mean()
         print("Mean : {}, Min : {}, Max : {}".format(pixel_mean, images.min(), images.max()))
         image = images - pixel_mean
         return images.mean(), np.array(image, dtype=np.float64)
 
-    def find_dicom_Countour_OTSU(self, hu_images, contour_boundary_value=100):
-        if hu_images.dtype != np.uint8:
-            hu_images = hu_images.astype(np.uint8)
-        thr, mask = cv2.threshold(hu_images, contour_boundary_value, 1, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        image, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    def find_dicom_Countour(self, binary_image):
+        if binary_image.dtype != np.uint8:
+            binary_image = binary_image.astype(np.uint8)
+        # plt.figure(None, figsize=(5, 3), dpi=200)
+        # plt.subplot(1, 2, 1)
+        # plt.axis('off')
+        # plt.tight_layout()
+        # plt.imshow(binary_image, cmap='bone')
+        # plt.title("Binary Image")
+        image, contours, hierarchy = cv2.findContours(binary_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # plt.subplot(1, 2, 2)
+        # plt.axis('off')
+        # plt.tight_layout()
+        # con = np.zeros([512,512,3])
+        # cv2.drawContours(con, contours, -1, (0, 0, 255), 1)
+        # plt.imshow(con, cmap='bone')
+        # plt.title("Contour")
+        # plt.show()
         return image, contours, hierarchy
 
-    def find_brain_contour(self, contours,hierarchy, init_position):
+    def get_binary_image_with_adaptiveThreshold(self, images, contour_boundary_value=100, block_size=11, c=2):
+        if images.dtype != np.uint8:
+            images = images.astype(np.uint8)
+        mask = cv2.adaptiveThreshold(images, contour_boundary_value, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,
+                                     block_size, c)
+        return mask
+
+    def get_binary_image_with_OTSU(self, images, contour_boundary_value=10):
+        if images.dtype != np.uint8:
+            images = images.astype(np.uint8)
+        thr, mask = cv2.threshold(images, contour_boundary_value, 1, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        return thr, mask
+
+    def get_binary_image_with_Canny(self, images, min_value=10, max_value=200):
+        if images.dtype != np.uint8:
+            images = images.astype(np.uint8)
+            images += images.min()
+        mask = cv2.Canny(images, min_value, max_value)
+        return mask
+
+    def find_brain_contour(self, contours, hierarchy, init_position):
         brain_contour = []
         for index, contour in enumerate(contours):
-            if cv2.pointPolygonTest(contour, init_position, measureDist=True) >= 0 and hierarchy[0][index][3] != -1:
-                if hierarchy[0][hierarchy[0][index][3]][3] == -1:
-                    brain_contour.append(contour)
+            if cv2.pointPolygonTest(contour, init_position, measureDist=True) >= 0 and hierarchy[0][index][3] == -1:
+                brain_contour.append(contour)
+
         return np.array(brain_contour)
 
     def extract_image_with_contour(self, image, brain_contour):
