71ad520a9a9aa8984bb2eef35c9bcf23289281c1
Gitko97 <xcvdsf8216@gmail.com>
2021-01-19 10:20:18 +0900

    ct brain contour 의 ui 약간 수정

--- ui/ui.py
+++ ui/ui.py
@@ -19,29 +19,34 @@ class Ui_Dialog(QtWidgets.QDialog):
 
     def setupUi(self):
         self.setObjectName("Dialog")
-        self.resize(800, 600)
+        self.resize(800, 800)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.frame = QtWidgets.QFrame(self)
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
+
         self.listView = QtWidgets.QListWidget(self.frame)
         self.listView.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
         self.listView.setGeometry(QtCore.QRect(0, 0, 781, 431))
         self.listView.setObjectName("listView")
+        self.listView.itemClicked.connect(self.controller.itemClicked)
 
         self.choose_directory_button = QtWidgets.QPushButton(self.frame)
         self.choose_directory_button.setGeometry(QtCore.QRect(30, 460, 201, 32))
         self.choose_directory_button.setObjectName("choose_directory_button")
+        self.choose_directory_button.clicked.connect(self.controller.load_files)
 
         self.selected_delete_button = QtWidgets.QPushButton(self.frame)
-        self.selected_delete_button.setGeometry(QtCore.QRect(130, 510, 201, 32))
+        self.selected_delete_button.setGeometry(QtCore.QRect(30, 510, 201, 32))
         self.selected_delete_button.setObjectName("selected_delete_button")
+        self.selected_delete_button.clicked.connect(self.controller.delete_files)
 
         self.add_file_button = QtWidgets.QPushButton(self.frame)
         self.add_file_button.setGeometry(QtCore.QRect(260, 460, 201, 32))
         self.add_file_button.setObjectName("add_file_button")
+        self.add_file_button.clicked.connect(self.controller.add_files)
 
         self.dicom_save_button = QtWidgets.QPushButton(self.frame)
         self.dicom_save_button.setGeometry(QtCore.QRect(490, 460, 201, 32))
@@ -49,21 +54,21 @@ class Ui_Dialog(QtWidgets.QDialog):
         self.dicom_save_button.clicked.connect(self.controller.file_save)
 
         self.setting_value_button = QtWidgets.QPushButton(self.frame)
-        self.setting_value_button.setGeometry(QtCore.QRect(410, 510, 201, 32))
+        self.setting_value_button.setGeometry(QtCore.QRect(260, 510, 201, 32))
         self.setting_value_button.setObjectName("setting_value")
+        self.setting_value_button.clicked.connect(self.controller.openSetting)
 
         self.change_TFRecord_button = QtWidgets.QPushButton(self.frame)
-        self.change_TFRecord_button.setGeometry(QtCore.QRect(300, 530, 201, 32))
+        self.change_TFRecord_button.setGeometry(QtCore.QRect(490, 510, 201, 32))
         self.change_TFRecord_button.setObjectName("tfRecord")
         self.change_TFRecord_button.clicked.connect(self.controller.change_dicom_to_tfRecord)
 
-        self.listView.itemClicked.connect(self.controller.itemClicked)
+        self.using_gan_button = QtWidgets.QPushButton(self.frame)
+        self.using_gan_button.setGeometry(QtCore.QRect(30, 560, 201, 32))
+        self.using_gan_button.setObjectName("gan")
+        self.using_gan_button.clicked.connect(self.controller.open_gan_view)
         # self.listView.itemDoubleClicked.connect(self.controller.itemDoubleClicked)
         # self.listView.currentItemChanged.connect(self.chkCurrentItemChanged)
-        self.choose_directory_button.clicked.connect(self.controller.load_files)
-        self.add_file_button.clicked.connect(self.controller.add_files)
-        self.selected_delete_button.clicked.connect(self.controller.delete_files)
-        self.setting_value_button.clicked.connect(self.controller.openSetting)
 
         self.horizontalLayout.addWidget(self.frame)
         self.setLayout(self.horizontalLayout)
@@ -80,3 +85,4 @@ class Ui_Dialog(QtWidgets.QDialog):
         self.setting_value_button.setText(_translate("Dialog", "기본 값 설정"))
         self.dicom_save_button.setText(_translate("Dialog", "자른 파일 저장"))
         self.change_TFRecord_button.setText(_translate("Dialog", "tfRecord저장"))
+        self.using_gan_button.setText(_translate("Dialog", "Gan 사용하기"))
