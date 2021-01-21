75912e585f10f835a309cafac887c9906d6a8a3f
Gitko97 <xcvdsf8216@gmail.com>
2021-01-21 16:25:21 +0900

    Cycle Gan 실행 방법 변경

--- model/dual_cycle_gan.py
+++ model/dual_cycle_gan.py
@@ -2,20 +2,21 @@ import keras
 import tensorflow as tf
 import numpy as np
 from loss import Loss
+from tensorflow.python.distribute.mirrored_strategy import MirroredStrategy
 from tensorflow.python.keras import Input
 from tensorflow.python.keras.layers import Reshape
 
 
-class CycleGan(keras.Model):
+class CycleGan:
     def __init__(
             self,
             generator_G,
             generator_F,
             discriminator_X,
             discriminator_Y,
-            lambda_cycle=10.0
+            batch_size,
+            lambda_cycle=10.0,
     ):
-        super(CycleGan, self).__init__()
         self.gen_G = generator_G
         self.gen_F = generator_F
         self.disc_X = discriminator_X
@@ -23,6 +24,7 @@ class CycleGan(keras.Model):
         self.lambda_cycle = lambda_cycle
         self.loss_function = Loss()
         self.number = 0
+        self.batch_size = batch_size
 
     def compile(
             self,
@@ -31,7 +33,6 @@ class CycleGan(keras.Model):
             disc_X_optimizer,
             disc_Y_optimizer
     ):
-        super(CycleGan, self).compile()
         self.gen_G_optimizer = gen_G_optimizer
         self.gen_F_optimizer = gen_F_optimizer
         self.disc_X_optimizer = disc_X_optimizer
@@ -44,72 +45,114 @@ class CycleGan(keras.Model):
         self.ssim_loss_fn = self.loss_function.ssim_loss_fn
 
     @tf.function  # change to graph mode
-    def train_step(self, ct, mr):
-        with tf.GradientTape(persistent=True) as tape:
-            # Horse to fake zebra
-            fake_mr = self.gen_G(ct, training=True)
-            # Zebra to fake horse -> y2x
-            fake_ct = self.gen_F(mr, training=True)
-            # Cycle (Horse to fake zebra to fake horse): x -> y -> x
-            cycled_x = self.gen_F(fake_mr, training=True)
-            # Cycle (Zebra to fake horse to fake zebra) y -> x -> y
-            cycled_y = self.gen_G(fake_ct, training=True)
+    def train_step(self, ct, mr, strategy: MirroredStrategy):
+        def calculate(ct, mr, batch_size):
+            with tf.GradientTape(persistent=True) as tape:
+                # CT to fake mri
+                fake_mr = self.gen_G(ct, training=True)
+                # MRI to fake CT
+                fake_ct = self.gen_F(mr, training=True)
 
-            # Discriminator output
-            disc_real_x = self.disc_X(ct, training=True)
-            disc_fake_x = self.disc_X(fake_ct, training=True)
+                # Cycle (CT to fake MRI to fake CT)
+                cycled_x = self.gen_F(fake_mr, training=True)
+                # Cycle (MRI to fake CT to fake MRI)
+                cycled_y = self.gen_G(fake_ct, training=True)
 
-            disc_real_y = self.disc_Y(mr, training=True)
-            disc_fake_y = self.disc_Y(fake_mr, training=True)
+                # Discriminator output
+                disc_real_x = self.disc_X(ct, training=True)
+                disc_fake_x = self.disc_X(fake_ct, training=True)
 
-            # Generator adverserial loss
-            gen_G_loss = self.generator_loss_fn(disc_fake_y)
-            gen_F_loss = self.generator_loss_fn(disc_fake_x)
+                disc_real_y = self.disc_Y(mr, training=True)
+                disc_fake_y = self.disc_Y(fake_mr, training=True)
 
-            # Generator cycle loss
-            cycle_loss_G = self.cycle_loss_fn(mr, cycled_y, self.lambda_cycle)
-            cycle_loss_F = self.cycle_loss_fn(ct, cycled_x, self.lambda_cycle)
+                # Generator adverserial loss
+                gen_G_loss = self.generator_loss_fn(disc_fake_y)
+                gen_F_loss = self.generator_loss_fn(disc_fake_x)
 
-            gradient_difference_loss_G = self.gradient_difference_loss(fake_mr, mr)
-            gradient_difference_loss_F = self.gradient_difference_loss(fake_ct, ct)
+                # Divide By batch_size because calculate with MultiGPU
+                gen_G_loss = tf.reduce_sum(gen_G_loss) * (1.0 / batch_size)
+                gen_F_loss = tf.reduce_sum(gen_F_loss) * (1.0 / batch_size)
 
-            ssim_loss_G = self.ssim_loss_fn(fake_mr, mr)
-            ssim_loss_F = self.ssim_loss_fn(fake_ct, ct)
+                # Generator cycle loss
+                cycle_loss_G = self.cycle_loss_fn(mr, cycled_y, self.lambda_cycle)
+                cycle_loss_F = self.cycle_loss_fn(ct, cycled_x, self.lambda_cycle)
 
-            perceptual_loss_G = self.perceptual_loss_fn(fake_mr, mr)
-            perceptual_loss_F = self.perceptual_loss_fn(fake_ct, ct)
+                # Divide By batch_size because calculate with MultiGPU
+                cycle_loss_G = tf.reduce_sum(cycle_loss_G) * (1.0 / batch_size)
+                cycle_loss_F = tf.reduce_sum(cycle_loss_F) * (1.0 / batch_size)
 
-            # Total generator loss
-            total_loss_G = cycle_loss_G + gradient_difference_loss_G + perceptual_loss_G + ssim_loss_G + gen_G_loss
-            total_loss_F = cycle_loss_F + gradient_difference_loss_F + ssim_loss_F + perceptual_loss_F + gen_F_loss
+                # Gradient loss
+                gradient_difference_loss_G = self.gradient_difference_loss(fake_mr, mr)
+                gradient_difference_loss_F = self.gradient_difference_loss(fake_ct, ct)
 
-            # Discriminator loss
-            disc_X_loss = self.discriminator_loss_fn(disc_real_x, disc_fake_x)
-            disc_Y_loss = self.discriminator_loss_fn(disc_real_y, disc_fake_y)
+                # Divide By batch_size because calculate with MultiGPU
+                gradient_difference_loss_G = tf.reduce_sum(gradient_difference_loss_G) * (1.0 / batch_size)
+                gradient_difference_loss_F = tf.reduce_sum(gradient_difference_loss_F) * (1.0 / batch_size)
 
-        # Get the gradients for the generators
-        grads_G = tape.gradient(total_loss_G, self.gen_G.trainable_variables)
-        grads_F = tape.gradient(total_loss_F, self.gen_F.trainable_variables)
+                # ssim loss
+                ssim_loss_G = self.ssim_loss_fn(fake_mr, mr)
+                ssim_loss_F = self.ssim_loss_fn(fake_ct, ct)
 
-        # Get the gradients for the discriminators
-        disc_X_grads = tape.gradient(disc_X_loss, self.disc_X.trainable_variables)
-        disc_Y_grads = tape.gradient(disc_Y_loss, self.disc_Y.trainable_variables)
+                # Divide By batch_size because calculate with MultiGPU
+                ssim_loss_G = tf.reduce_sum(ssim_loss_G) * (1.0 / batch_size)
+                ssim_loss_F = tf.reduce_sum(ssim_loss_F) * (1.0 / batch_size)
 
-        # Update the weights of the generators
-        self.gen_G_optimizer.apply_gradients(
-            zip(grads_G, self.gen_G.trainable_variables)
-        )
-        self.gen_F_optimizer.apply_gradients(
-            zip(grads_F, self.gen_F.trainable_variables)
-        )
+                # ssim loss
+                perceptual_loss_G = self.perceptual_loss_fn(fake_mr, mr)
+                perceptual_loss_F = self.perceptual_loss_fn(fake_ct, ct)
 
-        # Update the weights of the discriminators
-        self.disc_X_optimizer.apply_gradients(
-            zip(disc_X_grads, self.disc_X.trainable_variables)
-        )
-        self.disc_Y_optimizer.apply_gradients(
-            zip(disc_Y_grads, self.disc_Y.trainable_variables)
-        )
+                # VGG model feature loss
+                # perceptual_loss_G = tf.reduce_sum(perceptual_loss_G) * (1.0 / batch_size)
+                # perceptual_loss_F = tf.reduce_sum(perceptual_loss_F) * (1.0 / batch_size)
+
+                # Total generator loss
+                total_loss_G = cycle_loss_G + gradient_difference_loss_G + perceptual_loss_G + ssim_loss_G + gen_G_loss
+                total_loss_F = cycle_loss_F + gradient_difference_loss_F + ssim_loss_F + perceptual_loss_F + gen_F_loss
+
+                # Discriminator loss
+                disc_X_loss = self.discriminator_loss_fn(disc_real_x, disc_fake_x)
+                disc_Y_loss = self.discriminator_loss_fn(disc_real_y, disc_fake_y)
+
+                disc_X_loss = tf.reduce_sum(disc_X_loss) * (1.0 / batch_size)
+                disc_Y_loss = tf.reduce_sum(disc_Y_loss) * (1.0 / batch_size)
+
+            # Get the gradients for the generators
+            grads_G = tape.gradient(total_loss_G, self.gen_G.trainable_variables)
+            grads_F = tape.gradient(total_loss_F, self.gen_F.trainable_variables)
+
+            # Get the gradients for the discriminators
+            disc_X_grads = tape.gradient(disc_X_loss, self.disc_X.trainable_variables)
+            disc_Y_grads = tape.gradient(disc_Y_loss, self.disc_Y.trainable_variables)
+
+            # Update the weights of the generators
+            self.gen_G_optimizer.apply_gradients(
+                zip(grads_G, self.gen_G.trainable_variables)
+            )
+            self.gen_F_optimizer.apply_gradients(
+                zip(grads_F, self.gen_F.trainable_variables)
+            )
+
+            # Update the weights of the discriminators
+            self.disc_X_optimizer.apply_gradients(
+                zip(disc_X_grads, self.disc_X.trainable_variables)
+            )
+            self.disc_Y_optimizer.apply_gradients(
+                zip(disc_Y_grads, self.disc_Y.trainable_variables)
+            )
+
+            return {
+                "G_loss": total_loss_G,
+                "F_loss": total_loss_F,
+                "D_X_loss": disc_X_loss,
+                "D_Y_loss": disc_Y_loss,
+            }
+
+        history = strategy.run(
+            calculate, args=(ct, mr, self.batch_size,))  # call multi gpu calculate
+        total_loss_G = strategy.reduce(tf.distribute.ReduceOp.SUM, history['G_loss'], axis=None)
+        total_loss_F = strategy.reduce(tf.distribute.ReduceOp.SUM, history['F_loss'], axis=None)
+        disc_X_loss = strategy.reduce(tf.distribute.ReduceOp.SUM, history['D_X_loss'], axis=None)
+        disc_Y_loss = strategy.reduce(tf.distribute.ReduceOp.SUM, history['D_Y_loss'], axis=None)
 
         return {
             "G_loss": total_loss_G,
--- model/learning_sequence_2.py
+++ model/learning_sequence_2.py
@@ -3,11 +3,10 @@ import os
 import sys
 import time
 
-import keras
 import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
-
+# -*- coding: utf-8 -*-
 sys.path.append('../')
 from callback import GANMonitor, MyTensorBoard
 from discriminator import Discriminator
@@ -15,6 +14,7 @@ from dual_cycle_gan import CycleGan
 from generator import Generator
 from load_dataset import LoadDataSet
 
+# tf.debugging.set_log_device_placement(True)  # print gpu allocate
 seed = 0
 np.random.seed(seed)
 
@@ -49,49 +49,59 @@ if args.d is None:
 else:
     EPOCHS, BATCH_SIZE, loader = get_options_with_parameter()
 
+strategy = tf.distribute.MirroredStrategy()
+
+print("Number of devices: {}".format(strategy.num_replicas_in_sync))
+global_batch_size = (BATCH_SIZE *
+                     strategy.num_replicas_in_sync)
+
 # ------Now try to load DATA-------#
 
-# ToDo:Change to Tensorflow Dataset
-ct_data, mr_data = loader.change_to_pixel_array()
-CT_train, CT_test, MR_train, MR_test = train_test_split(ct_data, mr_data, test_size=0.1)
+CT_train, MR_train = loader.change_to_pixel_array()
 
 CT_train = CT_train.reshape(CT_train.shape + (1,))
-CT_test = CT_test.reshape(CT_test.shape + (1,))
 MR_train = MR_train.reshape(MR_train.shape + (1,))
-MR_test = MR_test.reshape(MR_test.shape + (1,))
 
 print("The Number of CT Train Data is {}".format(np.shape(CT_train)[0]))
 print("The Number of MR Train Data is {}".format(np.shape(MR_train)[0]))
-print("The Number of CT Test Data is {}".format(np.shape(CT_test)[0]))
-print("The Number of MR Test Data is {}".format(np.shape(MR_test)[0]))
 
-train_dataset_CT = tf.data.Dataset.from_tensor_slices(CT_train).cache().shuffle(
-    512).batch(BATCH_SIZE)
-train_dataset_MR = tf.data.Dataset.from_tensor_slices(MR_train).cache().shuffle(
-    512).batch(BATCH_SIZE)
+with strategy.scope():
+    train_dataset = tf.data.Dataset.from_tensor_slices((CT_train, MR_train)).shuffle(len(CT_train)).batch(
+        global_batch_size,
+        drop_remainder=True)
+    # Remain Batch Data removed!
+    dist_dataset = strategy.experimental_distribute_dataset(train_dataset)
 
+# Generator(name="TEST G", input_shape=inputShape).print_vars()
+# Discriminator(name="TEST D", input_shape=inputShape).print_vars()
+# s = input()
 # ------Make Cycle Gan(only for unpaired data)-------#
+with strategy.scope():
+    gen_G = Generator(name="Generator_G", input_shape=inputShape).model
+    gen_F = Generator(name="Generator_F", input_shape=inputShape).model
+    disc_X = Discriminator(name="Discriminator_X", input_shape=inputShape).model
+    disc_Y = Discriminator(name="Discriminator_Y", input_shape=inputShape).model
 
-gen_G = Generator(name="Generator_G", input_shape=inputShape).model
-gen_F = Generator(name="Generator_F", input_shape=inputShape).model
-disc_X = Discriminator(name="Discriminator_X", input_shape=inputShape).model
-disc_Y = Discriminator(name="Discriminator_Y", input_shape=inputShape).model
+    generator_g_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.9)
+    generator_f_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.9)
+    discriminator_x_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.9)
+    discriminator_y_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.9)
 
-generator_g_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.9)
-generator_f_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.9)
-discriminator_x_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.9)
-discriminator_y_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.9)
-cycle_gan_model = CycleGan(
-    generator_G=gen_G, generator_F=gen_F, discriminator_X=disc_X, discriminator_Y=disc_Y
-)
+    cycle_gan_model = CycleGan(
+        generator_G=gen_G, generator_F=gen_F, discriminator_X=disc_X, discriminator_Y=disc_Y,
+        batch_size=global_batch_size
+    )
 
-# Compile the model
-cycle_gan_model.compile(
-    gen_G_optimizer=generator_g_optimizer,
-    gen_F_optimizer=generator_f_optimizer,
-    disc_X_optimizer=discriminator_x_optimizer,
-    disc_Y_optimizer=discriminator_y_optimizer,
-)
+    # Compile the model
+    cycle_gan_model.compile(
+        gen_G_optimizer=generator_g_optimizer,
+        gen_F_optimizer=generator_f_optimizer,
+        disc_X_optimizer=discriminator_x_optimizer,
+        disc_Y_optimizer=discriminator_y_optimizer,
+    )
+
+
+
 
 # ------For train callback method-------#
 imgae_callback = GANMonitor(gen_G, dir_path="./generate_img/", test_dataset=CT_train)
@@ -99,53 +109,58 @@ imgae_callback = GANMonitor(gen_G, dir_path="./generate_img/", test_dataset=CT_t
 if not os.path.exists("./checkpoints"):
     os.mkdir("./checkpoints")
 checkpoint_path = "./checkpoints"
-ckpt = tf.train.Checkpoint(generator_g=gen_G,
-                           generator_f=gen_F,
-                           discriminator_x=disc_X,
-                           discriminator_y=disc_Y,
-                           generator_g_optimizer=generator_g_optimizer,
-                           generator_f_optimizer=generator_f_optimizer,
-                           discriminator_x_optimizer=discriminator_x_optimizer,
-                           discriminator_y_optimizer=discriminator_y_optimizer)
+with strategy.scope():
+    ckpt = tf.train.Checkpoint(generator_g=gen_G,
+                               generator_f=gen_F,
+                               discriminator_x=disc_X,
+                               discriminator_y=disc_Y,
+                               generator_g_optimizer=generator_g_optimizer,
+                               generator_f_optimizer=generator_f_optimizer,
+                               discriminator_x_optimizer=discriminator_x_optimizer,
+                               discriminator_y_optimizer=discriminator_y_optimizer)
 
-ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
-# if a checkpoint exists, restore the latest checkpoint.
-if ckpt_manager.latest_checkpoint:
-    ckpt.restore(ckpt_manager.latest_checkpoint)
-    print('Latest checkpoint restored!!')
+    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+    # if a checkpoint exists, restore the latest checkpoint.
+    if ckpt_manager.latest_checkpoint:
+        ckpt.restore(ckpt_manager.latest_checkpoint)
+        print('Latest checkpoint restored!!')
 
 loss_log_tensorboard = MyTensorBoard("./logs_tensorboard/")
 
-K = tf.keras.backend
+with strategy.scope():
+    @tf.function
+    def call_train(image_x, image_y, strategy):
+        history = cycle_gan_model.train_step(image_x, image_y, strategy)
+
+        return history
 
 
-def train():
-    for epoch in range(EPOCHS):
-        start = time.time()
+    def train():
+        for epoch in range(EPOCHS):
+            start = time.time()
 
-        n = 0
-        for image_x, image_y in tf.data.Dataset.zip((train_dataset_CT, train_dataset_MR)):
-            history = cycle_gan_model.train_step(image_x, image_y)
-            # K.clear_session() # this method can be solution for memory leak
-            if n % 1 == 0:
+            n = 0 + global_batch_size
+            for image_x, image_y in dist_dataset:
+                history = call_train(image_x, image_y, strategy)
+                # K.clear_session() # this method can be solution for memory leak
                 print("{} Epochs / {} batch History :".format(epoch + 1, n))
                 template = 'G_loss: {}, F_loss: {}, D_X_loss: {}, D_Y_loss Loss: {}'
-                print(template.format(history['G_loss'].numpy(),
-                                      history['F_loss'].numpy(),
-                                      history['D_X_loss'].numpy(),
-                                      history['D_Y_loss'].numpy()))
-            n += 1
+                print(template.format(history['G_loss'],
+                                      history['F_loss'],
+                                      history['D_X_loss'],
+                                      history['D_Y_loss']))
+                n += global_batch_size
 
-        if (epoch + 1) % 5 == 0:
-            ckpt_save_path = ckpt_manager.save()
-            imgae_callback(epoch)
-            print('Saving checkpoint for epoch {} at {}'.format(epoch + 1,
-                                                                ckpt_save_path))
+            if (epoch + 1) % 5 == 0:
+                ckpt_save_path = ckpt_manager.save()
+                imgae_callback(epoch)
+                print('Saving checkpoint for epoch {} at {}'.format(epoch + 1,
+                                                                    ckpt_save_path))
 
-        print('Time taken for epoch {} is {} sec\n'.format(epoch + 1,
-                                                           time.time() - start))
-        loss_log_tensorboard.write_log(history, epoch)
-
+            print('Time taken for epoch {} is {} sec\n'.format(epoch + 1,
+                                                               time.time() - start))
+            loss_log_tensorboard.write_log(history, epoch)
 
 print("Start Learning")
 train()
